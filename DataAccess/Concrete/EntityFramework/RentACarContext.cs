6ef9bc635cf937d863c130e9a5284376984fa39d
kardem11 <simgdem6@gmail.com>
2022-07-29 01:57:51 +0300

    updated..

--- DataAccess/Concrete/EntityFramework/RentACarContext.cs
+++ DataAccess/Concrete/EntityFramework/RentACarContext.cs
@@ -10,10 +10,7 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class RentACarContext:DbContext
     {
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Customer>().HasNoKey();
-        }
+      
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Database=RentACar;Integrated Security=True;");
@@ -21,9 +18,9 @@ namespace DataAccess.Concrete.EntityFramework
         public DbSet<Car> Cars { get; set; }
         public DbSet<Color> Colors { get; set; }
         public DbSet<Brand> Brands { get; set; }
-        public DbSet<Customer> Customer { get; set; }
-        public DbSet<User> User { get; set; }
-        public DbSet<Rental> Rental { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
         public DbSet<CarImage> CarImages { get; set; }
     }
 }
